78e191a8b6162e8c45a3809a79bfda29c6d9fa26
fras-k <fras.krzysztoff@gmail.com>
2024-04-04 05:07:54 +0200

    interface_sketch_2

--- zaxiscalibrating.py
+++ zaxiscalibrating.py
@@ -9,43 +9,57 @@ import sys
 
 Tk().withdraw()
 window = tk.Tk()
-window.geometry("400x300")
+window.geometry("380x470")
+window.resizable(width=False, height=False)
 window.title("gcode straightener")
 window.configure(bg="white")
 
+mod_text = "compute to modify"
+
 def window_destroy():
+
     sys.exit()
 
-def ask_fn(input_file, base_name):
+window.protocol("WM_DELETE_WINDOW", window_destroy)
+
+def ask_fn():
+    global input_file, base_name
     input_file = askopenfilename(title='Select file to calibrate')
     base_name = os.path.basename(input_file)
-    return input_file, base_name
+    fn_label.config(text=base_name)
 
 
-def ask_dir(base_name, output_file):
-
+def ask_dir():
+    global base_name, output_file
     path = askdirectory(title='Select output destination')
     os.chdir(path)
     output_file = f"calib_{base_name}"
-    return output_file
+    dir_label.config(text=path)
 
-def save_mline(input_file, output_file):
+
+def save_mline(x,y):
+    global input_file, output_file
     with open(input_file, "r") as infile, open(output_file, "w") as outfile:
         for line in infile:
-            modified_line = modify_gcode_line(line)
+            modified_line = modify_gcode_line(line,x,y)
             outfile.write(modified_line)
 
+# x_tgAlfa = (np.tan(np.deg2rad(0.4)))
+# y_tgAlfa = (np.tan(np.deg2rad(0.2)))
 
-x_tgAlfa = (np.tan(np.deg2rad(0.4)))
-y_tgAlfa = (np.tan(np.deg2rad(0.2)))
+# nx = 0.0
+# ny = 0.0
+# x = 0.0
+# y = 0.0
 
-nx = 0.0
-ny = 0.0
-x = 0.0
-y = 0.0
-
-def modify_gcode_line(line):
-    global nx, ny, x, y
+def modify_gcode_line(line, x_value, y_value):
+    # global nx, ny, x, y
+    global mod_text
+    nx, ny = 0, 0
+    x = float(x_value)
+    y = float(y_value)
+    x_tgAlfa = (np.tan(np.deg2rad(x)))
+    y_tgAlfa = (np.tan(np.deg2rad(y)))
 
     if line.startswith(";Z:"):
         match = re.match(r';Z:([\d.]+)', line)
@@ -115,7 +129,7 @@ def modify_gcode_line(line):
 
             x = x + nx
             return f"G1 Y{x:.6f} E-{e:.6f} F{f:.6f}\n"
-
+    mod_label.configure(text="calibrated")
     return line
 
 # input_file, base_name =  ask_fn()
@@ -123,21 +137,51 @@ def modify_gcode_line(line):
 # save_mline(input_file, output_file)
 
 # print(f"Plik G-code został zmodyfikowany i zapisany jako {output_file}.")
-input_file, base_name = " ", " "
-output_file = " "
+input_file, base_name, output_file = "","select input file", "select output directory"
 
-browse_button = tk.Button(window, text="select file", command=lambda:ask_fn(input_file, base_name),width=15)
-dir_button =    tk.Button(window, text="select output file", command=lambda: ask_dir(base_name, output_file),width=15)
-modify_button = tk.Button(window, text="compute", command=lambda: save_mline(input_file, output_file), width = 15)
+window_frame = tk.LabelFrame(window, text="parameters", bg="white")
+
+
+axis_frame = tk.LabelFrame(window_frame, text="Axis Values", bg="white")
+input_frame = tk.LabelFrame(window_frame, text="input", bg="white")
+output_frame = tk.LabelFrame(window_frame, text="output", bg="white")
+
+window_frame.grid(row = 0, column= 0, padx=10, pady=10, columnspan=2)
+
+
+axis_frame.grid(row=0, column=0, padx=10, pady=10)
+input_frame.grid(row=1, column=0, padx=10, pady=10)
+output_frame.grid(row=2, column=0, padx=10, pady=10)
+
+x_value = tk.Entry(axis_frame)
+
+y_value = tk.Entry(axis_frame)
+x = x_value
+y = y_value
+
+
+browse_button = tk.Button(input_frame, text="input file", command=lambda:ask_fn(),width=15)
+dir_button =    tk.Button(output_frame, text="output directory", command=lambda: ask_dir(),width=15)
+modify_button = tk.Button(window, text="compute", command=lambda: save_mline(x_value.get(), y_value.get()), width = 15)
 exit_button =   tk.Button(window, text="exit", command=lambda:window_destroy(),width=15)
-x_value = tk.Entry(window)
-y_value = tk.Entry(window)
 
-x_value.grid(row = 0, column = 0, padx = 10, pady = 10)
-y_value.grid(row = 1, column = 0, padx = 10, pady = 10)
+x_label = tk.Label(axis_frame, text="x axis:", bg = "white", anchor="w")
+y_label = tk.Label(axis_frame, text="y axis:", bg = "white")
+fn_label = tk.Label(input_frame, text=base_name,bg = "white", width = 20, anchor="e")
+dir_label = tk.Label(output_frame, text=output_file,bg = "white", width = 20, anchor="e")
+mod_label = tk.Label(window, text = mod_text, bg="white" )
+
+x_value.grid(row = 0, column = 1, padx = 10, pady = 10)
+y_value.grid(row = 1, column = 1, padx = 10, pady = 10)
 browse_button.grid(row = 2, column = 0, padx = 10, pady = 10)
 dir_button.grid(row = 3, column = 0, padx = 10, pady = 10)
 modify_button.grid(row = 4, column = 0, padx = 10, pady = 10)
 exit_button.grid(row = 5, column = 0, padx = 10, pady = 10)
+
+x_label.grid(row=0, column=0, sticky="w")
+y_label.grid(row=1, column=0, sticky = "w")
+fn_label.grid(row=2, column=1, sticky="e")
+dir_label.grid(row=3, column=1, sticky="e")
+mod_label.grid(row=4, column = 1, sticky = "w")
 
 window.mainloop()
